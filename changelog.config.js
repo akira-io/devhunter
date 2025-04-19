77bd2832724d03ec8dc66a07dae65d6958a5bef7
kid <kidiatoliny@akira-io.com>
2025-04-19 22:42:16 -0100

    chore: update conventional changelog configuration to use custom preset

---
+++ changelog.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    types: [
+        { type: 'feat', section: '✨ Features' },
+        { type: 'fix', section: '🐛 Bug Fixes' },
+        // qualquer outro tipo omitido aqui não aparece no changelog
+    ],
+};
