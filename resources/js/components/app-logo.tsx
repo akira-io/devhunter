bd916c687fcade6a1c79caa78ec66af08f15cbc1
kidiatoliny <48266788+kidiatoliny@users.noreply.github.com>
2025-04-20 21:20:44 +0000

    chore: format frontend

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -5,7 +5,7 @@ export default function AppLogo() {
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
                 {/*<AppLogoIcon className="size-5 fill-current text-white dark:text-black" />*/}
-                <TerminalIcon className="text-white size-4 dark:text-black" />
+                <TerminalIcon className="size-4 text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold">Dev Hunter</span>
