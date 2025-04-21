1b3b9c1318bf9fb6c529cb58f7a84439e80b524f
kid <kidiatoliny@akira-io.com>
2025-04-21 15:38:21 -0100

    feat: update DevCount component to shuffle user avatars and improve welcome page layout

--- resources/js/components/dev-count.tsx
+++ resources/js/components/dev-count.tsx
@@ -1,23 +1,28 @@
 import { Button } from '@/components/ui/button';
+import { User } from '@/types';
 import Avatar, { genConfig } from 'react-nice-avatar';
 
-export default function DevCount({ count = 0 }: { count: number }) {
+function shuffleArray<T>(array: T[]): T[] {
+    return array
+        .map((item) => ({ item, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ item }) => item);
+}
+
+export default function DevCount({ users }: { users: User[] }) {
+    const shuffledUsers = shuffleArray(users);
     return (
         <div className="bg-muted mt-2 flex items-center rounded-full">
             <div className="flex -space-x-3">
-                <Avatar className="h-10 w-10" {...genConfig()} />
-                <Avatar className="h-10 w-10" {...genConfig()} />
-                <Avatar className="h-10 w-10" {...genConfig()} />
-                <Avatar className="h-10 w-10" {...genConfig()} />
-                <Avatar className="h-10 w-10" {...genConfig()} />
-                <Avatar className="h-10 w-10" {...genConfig()} />
-                <Avatar className="h-10 w-10" {...genConfig()} />
+                {shuffledUsers.map((user) => (
+                    <Avatar key={user.email} className="border-muted bg-background h-10 w-10 border-2" {...genConfig(user.name || user.email)} />
+                ))}
             </div>
             <Button
                 variant="secondary"
                 className="text-muted-foreground hover:text-foreground flex items-center justify-center rounded-full bg-transparent px-3 text-xs shadow-none hover:bg-transparent"
             >
-                +{count} devs
+                +{users.length} devs
             </Button>
         </div>
     );
--- resources/js/components/dev-mini-card.tsx
+++ resources/js/components/dev-mini-card.tsx
@@ -12,13 +12,15 @@ export function DevMiniCard({ title, description, image }: DevCardMini) {
     const config = genConfig({ hairStyle: 'thick' });
     return (
         <Card className="w-full">
-            <CardContent className="flex items-start justify-center gap-4">
+            <CardContent className="flex w-full flex-1 items-start justify-center gap-4">
                 <Avatar className="h-20 w-auto">
                     {image ? <AvatarImage src={image} alt={title} className="h-20 w-20" /> : <AvatarGenenerator className="h-20 w-20" {...config} />}
                 </Avatar>
-                <div className="flex-1">
+                <div className="w-full">
                     <CardTitle className="mb-2 cursor-pointer text-xl">{title}</CardTitle>
-                    <CardDescription className="mb-4 text-sm">{description}</CardDescription>
+                    <CardDescription className="mb-4 text-sm">
+                        {description ?? <span className="text-muted">Bio indisponivel...</span>}
+                    </CardDescription>
                 </div>
             </CardContent>
         </Card>
