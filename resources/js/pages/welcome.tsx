f5b7027430aa3fc423864281e5354003b6146c88
kid <kidiatoliny@akira-io.com>
2025-04-26 17:15:45 -0100

    feat: update welcome component layout; adjust header positioning and improve content visibility

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -43,7 +43,7 @@ export default function Welcome({ users }: { users: DataProps }) {
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
             <div className="flex min-h-screen flex-col items-center justify-start bg-[#FDFDFC] p-6 text-[#1b1b18] lg:p-8 dark:bg-[#0a0a0a]">
-                <header className="mb-6 w-full text-sm not-has-[nav]:hidden md:max-w-[335px] lg:max-w-4xl">
+                <header className="fixed top-0 z-50 w-full bg-[#FDFDFC]/90 bg-red-500 p-4 text-sm backdrop-blur md:px-40 dark:bg-[#0a0a0a]/90">
                     <nav className="flex items-center justify-end gap-4">
                         <AppLogo />
                         <div className="flex-1" />
@@ -74,17 +74,15 @@ export default function Welcome({ users }: { users: DataProps }) {
                         )}
                     </nav>
                 </header>
-                <div className="flex w-full flex-col items-center justify-start opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-                    {!query && (
-                        <div className="mt-20 flex w-full flex-col items-center justify-center gap-2 md:max-w-4xl lg:max-w-6xl">
-                            <h1 className={'text-4xl font-bold dark:text-white'}>Dev Hunter 🇨🇻</h1>
-                            <p className={'mb-10 max-w-2xl text-center text-lg font-normal text-[#1b1b18] dark:text-[#EDEDEC]'}>
-                                O ponto de partida para inovação, colaboração e tecnologia em Cabo Verde. Um ecossistema digital onde projetos ganham
-                                vida e talento local encontra visibilidade global.
-                            </p>
-                            <DevCount users={users} />
-                        </div>
-                    )}
+                <div className="mb-50 flex w-full flex-col items-center justify-start opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
+                    <div className="mt-20 flex w-full flex-col items-center justify-center gap-2 md:max-w-4xl lg:max-w-6xl">
+                        <h1 className={'text-4xl font-bold dark:text-white'}>Dev Hunter 🇨🇻</h1>
+                        <p className={'mb-10 max-w-2xl text-center text-lg font-normal text-[#1b1b18] dark:text-[#EDEDEC]'}>
+                            O ponto de partida para inovação, colaboração e tecnologia em Cabo Verde. Um ecossistema digital onde projetos ganham vida
+                            e talento local encontra visibilidade global.
+                        </p>
+                        {!query && <DevCount users={users} />}
+                    </div>
                     <div className="my-10 w-full max-w-xl dark:text-white">
                         <div className="relative mb-10 md:mb-20">
                             <Input
