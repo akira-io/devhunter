59e6b33a9ff07085010f57222a77b3a51e82de49
kid <kidiatoliny@akira-io.com>
2025-04-30 03:47:29 -0100

    feat: enhance layout for ProfileCompletion component and improve responsiveness

--- resources/js/components/profile/ProfileCompletion.tsx
+++ resources/js/components/profile/ProfileCompletion.tsx
@@ -68,8 +68,8 @@ export function ProfileCompletion({ skills, professionalEducations }: ProfileCom
     const profileCompletionPercentage = calculateProfileCompletion(profileCompletion);
 
     return (
-        <section className="bg-card gradient effect rounded-lg border p-6">
-            <div className="mb-4 flex items-center justify-between">
+        <section className="bg-card gradient effect rounded-lg p-6">
+            <div className="mb-4 flex w-full items-center justify-between">
                 <div>
                     <h3 className="text-foreground text-lg font-semibold">
                         {profileCompletionPercentage < 100 ? 'Complete seu perfil' : 'Perfil Completo'}
--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -65,7 +65,7 @@ export default function Profile({ mustVerifyEmail, status, skills, highlightedSk
             <Head title="Definições Perfil" />
             <div className="bg-background flex flex-col gap-4 text-gray-200 md:flex-row md:p-8">
                 <aside className="bg-background flex w-full flex-shrink-0 flex-col items-center p-6 md:w-80">
-                    <div className="mb-4 md:hidden">
+                    <div className="mb-4 w-full md:hidden">
                         <ProfileCompletion professionalEducations={professionalEducations} skills={highlightedSkills} />
                     </div>
                     <Card className="gradient effect w-full items-center justify-center p-6 md:w-80">
