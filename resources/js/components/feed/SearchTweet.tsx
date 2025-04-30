b339e2b59627e850327fd0368dec4f7a5aa3c51d
kid <kidiatoliny@akira-io.com>
2025-04-30 03:29:59 -0100

    feat: implement command dialog for search functionality in SearchTweet component

--- resources/js/components/feed/SearchTweet.tsx
+++ resources/js/components/feed/SearchTweet.tsx
@@ -1,33 +1,81 @@
-import { Input } from '@/components/ui/input';
-import { Loader, SearchIcon } from 'lucide-react';
-import React from 'react';
+import { ArrowUpRightIcon, CircleFadingPlusIcon, FileInputIcon, FolderPlusIcon, SearchIcon } from 'lucide-react';
+import * as React from 'react';
 
-export function SearchTweet() {
-    const [searchLoading, setSearchLoading] = React.useState(false);
+import {
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from '@/components/ui/command';
+import { Link } from '@inertiajs/react';
 
-    function search(e: React.ChangeEvent<HTMLInputElement>) {
-        setSearchLoading(true);
-        console.log('searching...', e);
-        if (e.target.value.length > 0) {
-            setTimeout(() => {
-                setSearchLoading(false);
-            }, 1000);
-        }
-    }
+export default function SearchTweet() {
+    const [open, setOpen] = React.useState(false);
+
+    React.useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+            if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault();
+                setOpen((open) => !open);
+            }
+        };
+
+        document.addEventListener('keydown', down);
+        return () => document.removeEventListener('keydown', down);
+    }, []);
 
     return (
-        <div className="relative mr-2 mb-1">
-            <Input
-                type="text"
-                placeholder="procurar..."
-                className="placeholder:text-foreground-muted w-40 ps-9 pe-9 md:w-60 lg:w-80"
-                onChange={(e) => {
-                    search(e);
-                }}
-            />
-            <div className="text-muted-foreground pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
-                {!searchLoading ? <SearchIcon size={16} /> : <Loader className="animate-spin" size={16} />}
-            </div>
-        </div>
+        <>
+            <button
+                className="border-input bg-background text-foreground placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-9 w-fit rounded-md border px-3 py-2 text-sm shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px]"
+                onClick={() => setOpen(true)}
+            >
+                <span className="flex grow items-center">
+                    <SearchIcon className="text-muted-foreground/80 -ms-1 me-3" size={16} aria-hidden="true" />
+                    <span className="text-muted-foreground/70 font-normal">Search</span>
+                </span>
+                <kbd className="bg-background text-muted-foreground/70 ms-12 -me-1 inline-flex h-5 max-h-full items-center rounded border px-1 font-[inherit] text-[0.625rem] font-medium">
+                    ⌘K
+                </kbd>
+            </button>
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Type a command or search..." />
+                <CommandList>
+                    <CommandEmpty>No results found.</CommandEmpty>
+                    <CommandGroup heading="Quick start">
+                        <CommandItem>
+                            <FolderPlusIcon size={16} className="opacity-60" aria-hidden="true" />
+                            <span>Adicionar Story</span>
+                            <CommandShortcut className="justify-center">⌘N</CommandShortcut>
+                        </CommandItem>
+                        <CommandItem>
+                            <FileInputIcon size={16} className="opacity-60" aria-hidden="true" />
+                            <span>Novo Post</span>
+                            <CommandShortcut className="justify-center">⌘I</CommandShortcut>
+                        </CommandItem>
+                        <CommandItem>
+                            <CircleFadingPlusIcon size={16} className="opacity-60" aria-hidden="true" />
+                            <Link href={route('profile.edit')}>Perfil</Link>
+                            <CommandShortcut className="justify-center">⌘B</CommandShortcut>
+                        </CommandItem>
+                    </CommandGroup>
+                    <CommandSeparator />
+                    <CommandGroup heading="Navigation">
+                        <CommandItem>
+                            <ArrowUpRightIcon size={16} className="opacity-60" aria-hidden="true" />
+                            <Link href={route('home')}>Pagina Inicial</Link>
+                        </CommandItem>
+                        <CommandItem>
+                            <ArrowUpRightIcon size={16} className="opacity-60" aria-hidden="true" />
+                            <Link href={route('feed')}>Feed</Link>
+                        </CommandItem>
+                    </CommandGroup>
+                </CommandList>
+            </CommandDialog>
+        </>
     );
 }
