b11c17c92fe32522a70e5b7483e740611af7d16c
kid <kidiatoliny@akira-io.com>
2025-05-07 02:50:49 -0100

    chore: add Laravel Horizon package and schedule command for snapshots

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+final class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+        $this->gate();
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        Horizon::routeMailNotificationsTo('kidiatoliny@akira-io.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     */
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', function ($user) {
+            return in_array($user->email, [
+                'kidiatoliny@akira-io.com',
+                'kidiatoliny@gmail.com',
+            ]);
+        });
+    }
+}
