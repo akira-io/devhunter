1081437f63265292b3d2de5a1116eadbeeac2cfc
kid <kidiatoliny@akira-io.com>
2025-05-27 15:45:24 -0100

    refactor: enhance migration for auth logs table with strict types and return types

--- database/migrations/2025_05_23_215256_create_laravel_auth_logs_table.php
+++ database/migrations/2025_05_23_215256_create_laravel_auth_logs_table.php
@@ -1,14 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
-        Schema::create( config('auth-logs.table_name'), function (Blueprint $table) {
+        Schema::create(config('auth-logs.table_name'), function (Blueprint $table): void {
             $table->id();
             $table->morphs('authenticatable');
             $table->string('ip_address', 45)->nullable();
